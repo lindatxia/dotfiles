6f8d9dbf5b62515926d79ad68f4d33632b6822ed
Linda Xia <linda.xia@gusto.com>
2024-06-07 21:30:01 -0400

    first commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,4 @@
+abbr -a -- dbm bin/rails db:migrate
+abbr -a -- rc bin/rails console
+abbr -a -- rs bin/rails server
+abbr -a -- prep "git pull && bundle && yarn && bin/rails db:migrate && say done"
