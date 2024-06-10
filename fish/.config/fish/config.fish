42ded5a8b30b2ebb3068ba1db717e937c492030f
Linda Xia <linda.xia@gusto.com>
2024-06-09 20:23:46 -0400

    config with stow

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,15 @@
-abbr -a -- dbm bin/rails db:migrate
-abbr -a -- rc bin/rails console
-abbr -a -- rs bin/rails server
-abbr -a -- prep "git pull && bundle && yarn && bin/rails db:migrate && say done"
+abbr -a -- dbm 	bin/rails db:migrate
+abbr -a -- rc 	bin/rails console
+abbr -a -- rs 	bin/rails server
+abbr -a -- prep	git pull && bundle && yarn && bin/rails db:migrate && say done
+
+abbr -a -- gs 	git status
+abbr -a -- ga	git add
+abbr -a -- gd	git diff
+abbr -a -- gcm 	git commit -m
+
+abbr -a -- wh	cd ~/workspace/webhooks
+abbr -a -- dp	cd ~/workspace/developer-portal
+abbr -a -- fl	cd ~/workspace/gws-flows
+abbr -a -- zp	cd ~/workspace/zenpayroll
+abbr -a -- docs	cd ~/workspace/Gusto-Partner-API
