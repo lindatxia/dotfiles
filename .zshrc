82dd5b587d0d36b6228ff2d4c5618a0903be5461
Linda Xia <linda.xia@gusto.com>
2024-10-29 20:54:02 -0400

    Backup latest

---
+++ .zshrc
@@ -0,0 +1,2 @@
+alias ga='f() { bin/rubocop --autocorrect --force-exclusion $1; git add $1 };f'
+alias zp='cd ~/workspace/zenpayroll'
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,14 @@
-abbr -a -- dbm 	bin/rails db:migrate
-abbr -a -- rc 	bin/rails console
-abbr -a -- rs 	bin/rails server
-abbr -a -- prep	"git pull && bundle && yarn && bin/rails db:migrate && say done"
+# abbr -a -- dbm 	bin/rails db:migrate
+# abbr -a -- rc 	bin/rails console
+# abbr -a -- rs 	bin/rails server
+# abbr -a -- prep	"git pull && bundle && yarn && bin/rails db:migrate && say done"
 
 abbr -a -- gs 	git status
 abbr -a -- ga	git add
 abbr -a -- gd	git diff
 abbr -a -- gcm 	git commit -m
 abbr -a -- gb	git branch --sort=-committerdate
+abbr -a -- gco 	git checkout
 
 abbr -a -- wh	cd ~/workspace/webhooks
 abbr -a -- dp	cd ~/workspace/developer-portal
@@ -16,5 +17,5 @@ abbr -a -- zp	cd ~/workspace/zenpayroll
 abbr -a -- docs	cd ~/workspace/Gusto-Partner-API
 
 function fish_greeting
-   echo 👋 hi friend!
+   echo 👋 hi friend! you are fishin now 🐟
 end
